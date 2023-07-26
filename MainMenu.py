f4a30db265e66851a84a819097c31255d0ce7b42
yookiyoh <ralphlorenzicodilan@iskolarngbayan.pup.edu.ph>
2023-07-26 15:05:06 +0800

    initial commit + introductory headlines + general pseudocode

---
+++ MainMenu.py
@@ -0,0 +1,31 @@
+# Ralph Lorenz I. Codilan
+# BSCpE 1-5
+# Object-Oriented Programming | Final Project
+
+# Pseudocode
+
+'''
+# Import required libraries
+
+# Create the MainMenu class
+    # Initialize the main menu window
+    # Set up the theme setting
+    # Define methods to toggle themes, show introduction, and display message boxes
+    # Handle the start contact tracing button click
+        # Close the main menu window
+        # Open the ContactTracingApp window
+
+# Create the ContactTracingApp class
+    # Initialize the contact tracing app window
+    # Create a connection to the SQLite database and create a table to store data
+    # Set up UI elements: labels, entry fields, checkboxes, and buttons
+    # Implement methods to add entry, search entry, clear fields, clear checkboxes
+    # Handle the closeEvent method to prompt a confirmation dialog
+    # Define a method to show error messages
+
+# Main function
+    # Initialize the PyQt5 application
+    # Create and show the MainMenu window
+    # Run the application
+'''
+
